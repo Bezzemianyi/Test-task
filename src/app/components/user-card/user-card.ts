ae5019bbef5a4c392db3d00e21606da0d5d053e6
Dima_Bezzemianyi <125562777+Bezzemianyi@users.noreply.github.com>
2025-07-15 11:32:08 +0300

    fix

--- src/app/components/user-card/user-card.ts
+++ src/app/components/user-card/user-card.ts
@@ -13,18 +13,18 @@ export class UserCard {
   @Input() user!: User;
 
   getCurrentTemperature(): number {
-    return this.user.weather?.current_weather?.temperature ?? 0; // Возвращаем температуру или 0 по умолчанию
+    return this.user.weather?.current_weather?.temperature ?? 0;
   }
 
   getWeatherIcon(weatherCode: number): string {
     const weatherMap = [
-      { codes: [0], icon: 'assets/icons/weather/sunny.png' }, // Clear sky
-      { codes: [1, 2, 3], icon: 'assets/icons/weather/cloudy.png' }, // Mainly clear, partly cloudy, overcast
-      { codes: [45, 48], icon: 'assets/icons/weather/fog.png' }, // Fog, depositing rime fog
-      { codes: [51, 53, 55], icon: 'assets/icons/weather/drizzle.png' }, // Drizzle
-      { codes: [61, 63, 65], icon: 'assets/icons/weather/rain.png' }, // Rain
-      { codes: [71, 73, 75], icon: 'assets/icons/weather/snow.png' }, // Snow fall
-      { codes: [95], icon: 'assets/icons/weather/thunder.png' }, // Thunderstorm
+      { codes: [0], icon: 'assets/icons/weather/sunny.png' },
+      { codes: [1, 2, 3], icon: 'assets/icons/weather/cloudy.png' },
+      { codes: [45, 48], icon: 'assets/icons/weather/fog.png' },
+      { codes: [51, 53, 55], icon: 'assets/icons/weather/drizzle.png' },
+      { codes: [61, 63, 65], icon: 'assets/icons/weather/rain.png' },
+      { codes: [71, 73, 75], icon: 'assets/icons/weather/snow.png' },
+      { codes: [95], icon: 'assets/icons/weather/thunder.png' },
     ];
     for (const weather of weatherMap) {
       if (weather.codes.includes(weatherCode)) {
